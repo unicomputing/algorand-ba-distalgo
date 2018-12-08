f6b64cae84e8d8f4de307f2d52b02785f22dfcf3
Sai Parthasarathy <sai.miduthuri@stonybrook.edu>
2018-12-08 18:05:36 -0500

    Changed parameters to be a class of values, rather than importing directly from file.

--- src/parameters.py
+++ src/parameters.py
@@ -1,17 +1,28 @@
-lambda_block = 4
+# lambda_block = 4
+#
+# lambda_step = 4
+#
+# T_final = 0.74
+#
+# T_step = 0.685
+#
+# tau_step = 5
+#
+# tau_proposer = 2
+#
+# tau_final = 6
+#
+# max_steps = 50
+#
+# logger_level = 20
 
-lambda_step = 4
-
-T_final = 0.74
-
-T_step = 0.685
-
-tau_step = 5
-
-tau_proposer = 2
-
-tau_final = 6
-
-max_steps = 50
-
-logger_level = 20
+class Parameters:
+    lambda_block = 4
+    lambda_step = 4
+    T_final = 0.74
+    T_step = 0.685
+    tau_step = 5
+    tau_proposer = 2
+    tau_final = 6
+    max_steps = 50
+    logger_level = 20
