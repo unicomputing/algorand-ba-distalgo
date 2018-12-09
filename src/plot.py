770dd6265629e310ff294a372d7a9343cb1f79de
kchetan <kasireddychetan@gmail.com>
2018-12-09 16:34:59 -0500

    code for plots

---
+++ src/plot.py
@@ -0,0 +1,20 @@
+import json
+import matplotlib.pyplot as plt
+import os
+
+
+if os.path.exists("temp.json"):
+	f = open('temp.json','r')
+	for line in f:
+		line = line[:-1]
+		obj = json.loads(line)
+		fig, ax = plt.subplots()
+		ax.plot(obj['x'], obj['y'])
+		ax.set(xlabel= obj['xlabel'], ylabel= obj['ylabel'], title= obj['title'])
+		ax.set_ylim(ymin=0)
+		ax.grid()
+		filename = obj['title'].replace(" % ","").replace(" ","_")
+		fig.savefig("performance_graphs/" + filename + ".png")
+	#os.remove("temp.json")
+else:
+  print("The file does not exist")
