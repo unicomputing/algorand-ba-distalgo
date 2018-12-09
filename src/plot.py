68b1ed3f2c2667e77a47678f6bdd796d085dcefa
Sai Parthasarathy <sai.miduthuri@stonybrook.edu>
2018-12-09 17:54:12 -0500

    Small changes to make monitor work.

--- src/plot.py
+++ src/plot.py
@@ -11,7 +11,7 @@ if os.path.exists("temp.json"):
 		fig, ax = plt.subplots()
 		ax.plot(obj['x'], obj['y'])
 		ax.set(xlabel= obj['xlabel'], ylabel= obj['ylabel'], title= obj['title'])
-		ax.set_ylim(ymin=0)
+		ax.set_ylim(ymin=0, ymax= max(obj['y']) + max(obj['y']) - min(obj['y']))
 		ax.grid()
 		filename = obj['title'].replace(" % ","").replace(" ","_")
 		fig.savefig("performance_graphs/" + filename + ".png")
