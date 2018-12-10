602a4043c4b660de3e54285a4b44adb8e1546740
kchetan <kasireddychetan@gmail.com>
2018-12-09 19:11:15 -0500

    documentation parameters

--- src/parameters.py
+++ src/parameters.py
@@ -1,4 +1,20 @@
 class Parameters:
+    """
+    The following parameters are parameters.
+
+    lambda_block = Timeout for receiving a block
+    lambda_step = Timeout for BA* step
+    T_final = Threshold of tau final for BA*
+    T_step = Threshold of tau step for BA*
+    tau_step = Expected # of committee members
+    tau_proposer = Expected # of block proposers
+    tau_final = Expected # of final committee members
+    max_steps = Maximum number of steps in BinaryBA*
+    logger_level = The logging level of the whole program.
+    loss_rate = The loss rate of the messages.
+    msg_delay = Delay inreceiving the message
+
+    """
     lambda_block = 10
     lambda_step = 10
     T_final = 0.74
